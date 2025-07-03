6da8311fd7a09bad15d3709b8f692261e7c72c31
sapolayot <sapolayot.nif@gmail.com>
2025-07-03 19:46:33 +0700

    Update dockerfile

--- book-management-frontend/nuxt.config.ts
+++ book-management-frontend/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     runtimeConfig: {
         public: {
-            apiBase: process.env.API_BASE,
+            apiBase: process.env.API_BASE || "http://localhost:8000",
         },
     },
     modules: ["@nuxtjs/tailwindcss"],
